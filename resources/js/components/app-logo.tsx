a031afe4272ce46a3c6e1865c87cde7eb8c613be
Jorge221z <7460062@alu.murciaeduca.es>
2025-03-28 20:28:58 +0100

    Design upgrade and logo change

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,13 +2,31 @@ import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
-        <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+        <div className="flex items-center">
+            <div className="
+                bg-sidebar-primary
+                text-sidebar-primary-foreground
+                flex
+                aspect-square
+                size-10
+                items-center
+                justify-center
+                rounded-xl
+                overflow-visible
+                p-0
+            ">
+                <AppLogoIcon className="
+                    size-full
+                    fill-current
+                    text-white
+                    dark:text-black
+                " />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5  leading-none font-semibold text-xl">script2me</span>
+            <div className="ml-3 grid flex-1 text-left">
+                <span className="text-xl font-semibold leading-none">
+                    script2me
+                </span>
             </div>
-        </>
+        </div>
     );
 }
--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -45,7 +45,7 @@ export function AppSidebar() {
     ];
 
     return (
-        <Sidebar collapsible="icon" variant="inset">
+        <Sidebar collapsible="icon" variant="inset" className="side">
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
