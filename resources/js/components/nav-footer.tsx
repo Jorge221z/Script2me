e16bb76260255034f5b26e5026776b9797124475
Jorge221z <7460062@alu.murciaeduca.es>
2025-03-29 11:08:20 +0100

    Sidebar text color fixed for theme changes

--- resources/js/components/nav-footer.tsx
+++ resources/js/components/nav-footer.tsx
@@ -18,7 +18,7 @@ export function NavFooter({
                         <SidebarMenuItem key={item.title}>
                             <SidebarMenuButton
                                 asChild
-                                className="text-neutral-600 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"
+                                className="text-gray-300 hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100"
                             >
                                 <a
                                     href={item.href}
