8ffcbe4a05e3333047fbf479b9abb9076efaaf0e
Jorge221z <jorgemunozcast12@gmail.com>
2025-05-03 20:43:16 +0200

    Some UI fixes

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -94,7 +94,7 @@ export default function Dashboard() {
             <Head title={t('dashboard.headTitle')} />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
 
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl">
+                <div className="border-sidebar-border/70 dark:border-sidebar-border relative rounded-xl pb-6">
                     {/* Formulario */}
                     <UploadForm
                         actionUrl="/upload"
@@ -106,7 +106,7 @@ export default function Dashboard() {
                     />
                 </div>
                 {/* Zona de vista previa */}
-                <div className="border-sidebar-border dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min p-4 overflow-y-auto">
+                <div className="border-sidebar-border dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min p-4 overflow-y-auto ml-6 mr-6">
                     {/* Botones de acción */}
                     <div className="flex justify-between mb-4 items-center">
                         <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
